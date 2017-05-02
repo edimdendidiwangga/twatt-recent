b134fd2d14f8051b0b994f5944d26b23a6901262
edimdendidiwangga <edimdendy@gmail.com>
2017-05-02 11:55:52 +0700

    add cors

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -5,12 +5,13 @@ methods.searchTweets = function(req, res) {
   let search = req.params.search;
   model.getOauthTwitter(function(oauth){
     oauth.get(
-        `https://api.twitter.com/1.1/search/tweets.json?q=${search}`,
+        `https://api.twitter.com/1.1/search/tweets.json?q=${search}&result_type=recent&count=2`,
         process.env.Access_Token, //test user token
         process.env.Access_Token_Secret, //test user secret
         function (e, data){
           if (e) console.error(e);
-          res.send(data)
+          let status = JSON.parse(data)
+          res.send(status)
         })
     })
 }
@@ -18,12 +19,14 @@ methods.searchTweets = function(req, res) {
 methods.timelineTweets = function(req, res) {
   model.getOauthTwitter(function(oauth){
   oauth.get(
-      'https://api.twitter.com/1.1/statuses/home_timeline.json',
+      //'https://api.twitter.com/1.1/statuses/home_timeline.json',
+      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=edim_dendy&count=2',
       process.env.Access_Token, //test user token
       process.env.Access_Token_Secret, //test user secret
       function (e, data){
         if (e) console.error(e);
-        res.send(data)
+        let timeline = JSON.parse(data)
+        res.send(timeline)
       })
   })
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+var cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.set('port', process.env.PORT || 3000)
 
 app.use('/', require('./routes/index'));
